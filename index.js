794ed09cbe0ee7c1f8dafc31f11575b48890f3de
RunyararoMucheche <your_email@example.com>
2025-04-19 12:12:14 -0400

    Add starter code and starting route

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const User = require("./models/user");
+
+mongoose.connect("mongodb://127.0.0.1:27017/authDemo");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Database is Connected!");
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  console.log(req.body);
+});
+
+app.get("/secret", (req, res) => {
+  res.send("THIS IS A SECRET PAGE! IT WON'T BE HERE IN THE FUTURE");
+});
+
+app.listen(3030, () => {
+  console.log("LISTENING ON PORT 3030");
+});
