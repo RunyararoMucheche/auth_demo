3cd9e030551a1030eba44e9193c96ce21c433c20
RunyararoMucheche <your_email@example.com>
2025-04-19 14:48:37 -0400

    Create login.ejs and create login routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const User = require("./models/user");
+const bcrypt = require("bcrypt");
 
 mongoose.connect("mongodb://127.0.0.1:27017/authDemo");
 
@@ -17,11 +18,27 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("THIS IS THE HOME PAGE");
+});
+
 app.get("/register", (req, res) => {
   res.render("register");
 });
 
 app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  const user = new User({ username, password: hash });
+  await user.save();
+  res.redirect("/");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
   console.log(req.body);
 });
 
